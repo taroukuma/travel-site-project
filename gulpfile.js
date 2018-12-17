5787b1b8e351e7bf71c9f461d0537a3de7168deb
kumar surisetti <kumar.surisetti@mail.utoronto.ca>
2018-12-17 00:56:59 -0500

    Created the css workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch')
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    simplevars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    prefixer = require('autoprefixer')
 
-gulp.task('default', function() {
-    console.log('Hello. Gulp seems to work! Good job!')
+gulp.task('default', function () {
+    return console.log('Hello. Gulp seems to work! Good job!')
 })
 
-gulp.task('html', function() {
+gulp.task('html', function () {
     console.log('Something changed with the html files.')
 })
 
-gulp.task('css', function() {
-    console.log('Something changed with the css files.')
+gulp.task('css', function () {
+    return gulp.src('./app/assets/styles/*.css')
+        .pipe(postcss([simplevars, nested, prefixer]))
+        .pipe(gulp.dest('./app/temp/styles/'))
 })
 
-gulp.task('watch', function() {
-    
-    watch('./app/**/*.html', function() {
+gulp.task('watch', function () {
+
+    watch('./app/**/*.html', function () {
         gulp.start('html')
     })
 
-    watch('./app/assets/**/*.css', function() {
+    watch('./app/assets/**/*.css', function () {
         gulp.start('css')
     })
 })
